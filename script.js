cdff1251713d2eb51d6fce5b3daa92cf65b15f19
Lorem <126567635+LoremIpsumDoggo@users.noreply.github.com>
2023-03-22 21:46:28 +0800

    Update script.js
    Changed it so some statements have singular nouns, and removed some random consoles. Also corrected the condition for if two numbers are the biggest number.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var time;
 var hour;
 var mins;
 
+
 //Randomize numbers
     num1 = Math.ceil(Math.random() * 20);
     num2 = Math.ceil(Math.random() * 20);
@@ -21,72 +22,94 @@ var mins;
     mins = time % 60;
     hour = parseInt(time / 60);
 
+
 //Change the statements
-{
-    document.getElementById("p1").innerHTML = "I opened my laptop " + num1 + " times."; //paragraph1
-    document.getElementById("p2").innerHTML = "I drank a total of " + num2 + " liters of water."; //paragraph2
-    //paragraph3
-    switch (num3) {
-        case 1:
-            document.getElementById("p3").innerHTML = "I looked up 1 word in my Filipino-English dictionary, and found 0 definitions for it.";
-            break;
-    
-        default:
-            document.getElementById("p3").innerHTML = "I looked up " + num3 + " words in my Filipino-English dictionary, and found 0 definitions for them.";
-            break;
-    }
-    //paragraph4
-    var par4 = document.getElementById("p4");
-    switch (biggestnum) { //So it doesn't get cramped, I made the document.getElement etc. into a variable
-        case 1:
-            par4.innerHTML = "I also played 1 video game, surprisingly matching with ";
-            break;
-    
-        default:
-            par4.innerHTML = "I also played " + biggestnum + " different video games, coincidentally matching with ";
-            break;
-    }
-    
-    if((num1 == num2) && (num1 == num3)) {
-        par4.innerHTML += "all of the numbers above.";
-        console.log("asdadsasdasdadsadasdw");
-    }
-    else if((num1 == num2) || (num2 == num3) || (num1 == num3)) {
-        if(num1 == num2)
-        {
-            par4.innerHTML += "how many times I opened my laptop, and how much liters of water I drank.";
-            console.log("asdadsasdasdadsadasdw");
-        }
-        if(num2 == num3)
-        {
-            par4.innerHTML += "how much liters of water I drank, and how many words I looked up in my Filipino-English dictionary.";
-            console.log("asdadsasdasdadsadasdw");
-        }
-        if(num1 == num3)
-        {
-            par4.innerHTML += "how many times I opened my laptop, and how many words I looked up in my Filipino-English dictionary.";
-            console.log("asdadsasdasdadsadasdw");
-        }
-    }
-    else {
-        switch (biggestnum) {
-            case num1:
-                par4.innerHTML += "how many times I opened my laptop.";
-                console.log("asdadsasdasdadsadasdw");
-                break;
-            case num2:
-                par4.innerHTML += "how much liters of water I drank.";
-                console.log("asdadsasdasdadsadasdw");
-                break;
-            case num3:
-                par4.innerHTML += "how many words I looked up in my Filipino-English dictionary.";
-                console.log("asdadsasdasdadsadasdw");
+    //paragraph1
+        switch (num1) {
+            case 1:
+                document.getElementById("p1").innerHTML = "I opened my laptop 1 time.";
                 break;
         
             default:
+                document.getElementById("p1").innerHTML = "I opened my laptop " + num1 + " times.";
                 break;
         }
-    }
-    document.getElementById("p5").innerHTML = "The first letter I typed on each day is the letter \"" + numtolet + "\"."; //paragraph5
-    document.getElementById("p6").innerHTML = "I spent " + time + " minutes, or " + hour + " hours and " + mins + " minutes working on requirements."; //paragraph6
-}
+    //paragraph2
+        switch (num2) {
+            case 1:
+                document.getElementById("p2").innerHTML = "I drank a total of 1 liter of water.";
+                break;
+        
+            default:
+                document.getElementById("p2").innerHTML = "I drank a total of " + num2 + " liters of water.";
+                break;
+        }
+    //paragraph3
+        switch (num3) {
+            case 1:
+                document.getElementById("p3").innerHTML = "I looked up 1 word in my Filipino-English dictionary, and found 0 definitions for it.";
+                break;
+        
+            default:
+                document.getElementById("p3").innerHTML = "I looked up " + num3 + " words in my Filipino-English dictionary, and found 0 definitions for them.";
+                break;
+        }
+    //paragraph4
+        var par4 = document.getElementById("p4");
+        switch (biggestnum) { //So it doesn't get cramped, I made the document.getElement etc. into a variable
+            case 1:
+                par4.innerHTML = "I also played 1 video game, coincidentally matching with the highest number above, which is ";
+                break;
+        
+            default:
+                par4.innerHTML = "I also played " + biggestnum + " different video games, coincidentally matching with the highest number above, which is ";
+                break;
+        }
+        
+        if((num1 == num2) && (num1 == num3)) {
+            par4.innerHTML += "all of the numbers above.";
+        }
+        else if(((biggestnum == num1) && (biggestnum == num2)) ||
+                ((biggestnum == num2) && (biggestnum == num3)) ||
+                ((biggestnum == num1) && (biggestnum == num3))) {
+            if(num1 == num2)
+            {
+                par4.innerHTML += "how many times I opened my laptop, and how much liters of water I drank.";
+            }
+            if(num2 == num3)
+            {
+                par4.innerHTML += "how much liters of water I drank, and how many words I looked up in my Filipino-English dictionary.";
+            }
+            if(num1 == num3)
+            {
+                par4.innerHTML += "how many times I opened my laptop, and how many words I looked up in my Filipino-English dictionary.";
+            }
+        }
+        else {
+            switch (biggestnum) {
+                case num1:
+                    par4.innerHTML += "how many times I opened my laptop.";
+                    break;
+                case num2:
+                    par4.innerHTML += "how much liters of water I drank.";
+                    break;
+                case num3:
+                    par4.innerHTML += "how many words I looked up in my Filipino-English dictionary.";
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    //paragraph5
+        document.getElementById("p5").innerHTML = "The first letter I typed on each day is the letter \"" + numtolet + "\".";
+    //paragraph6
+        switch (hour) {
+            case 1:
+                document.getElementById("p6").innerHTML = "I spent " + time + " minutes, or 1 hr and " + mins + " min working on requirements.";
+                break;
+        
+            default:
+                document.getElementById("p6").innerHTML = "I spent " + time + " minutes, or " + hour + " hrs and " + mins + " min working on requirements.";
+                break;
+        }
